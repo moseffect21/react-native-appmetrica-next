1b28280d3400a9d8c46eb5be3468e463d52011d9
moseffect21 <moseffect21@gmail.com>
2023-03-23 16:00:10 +0300

    fix

--- android/src/main/java/com/yandex/metrica/plugin/reactnative/AppMetricaModule.java
+++ android/src/main/java/com/yandex/metrica/plugin/reactnative/AppMetricaModule.java
@@ -184,7 +184,7 @@ public class AppMetricaModule extends ReactContextBaseJavaModule {
         ECommercePrice actualPrice = new ECommercePrice(new ECommerceAmount(Integer.parseInt(params.getString("price")), params.getString("currency")));
         ECommerceProduct product = new ECommerceProduct(params.getString("sku")).setActualPrice(actualPrice).setName(params.getString("name"));
         if (params.hasKey("categoriesPath")) {
-            product.setCategoriesPath(params.getMap("categoriesPath").toList());
+            product.setCategoriesPath(params.getArray("categoriesPath").toList());
         }
         // if (params.hasKey("payload")) {
         //     product.setPayload(Utils.toJSONObject(params.getMap("payload")));
